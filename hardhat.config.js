435fef0be74239ff9f16646cf0ffb03405c71cbb
Max <max@Allens-MacBook-Pro.local>
2021-11-24 21:14:39 +0100

    _

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-waffle";
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
@@ -10,12 +10,10 @@ const ALCHEMY_API_KEY = "d5773857b7774540a81f201b7ce2055f";
 // Be aware of NEVER putting real Ether into testing accounts
 const ROPSTEN_PRIVATE_KEY = "1193c06eaf49cd119411c1508ada0b28ea5bdfe142aa08f745ade89110f1d5c9";
 
-module.exports = {
-  solidity: "0.7.3",
-  networks: {
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/d5773857b7774540a81f201b7ce2055f`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-    }
+export const solidity = "0.7.3";
+export const networks = {
+  ropsten: {
+    url: `https://ropsten.infura.io/v3/d5773857b7774540a81f201b7ce2055f`,
+    accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
   }
 };
